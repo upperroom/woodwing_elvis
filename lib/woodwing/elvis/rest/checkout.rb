deeb1bd720ac019a0722f4245f41b4604b0cf44b
David Posey <dgposey@gmail.com>
2017-04-11 13:37:47 -0500

    fix checkout and undo_checkout; document properly

--- lib/woodwing/elvis/rest/checkout.rb
+++ lib/woodwing/elvis/rest/checkout.rb
@@ -3,17 +3,28 @@ module WoodWing
     module Rest
 
 
-      # https://elvis.tenderapp.com/kb/api/rest-checkout
-      def checkout(options={})
-        url = base_url + "checkout"
+      # Check out an asset.  Optionally download it, too.
+      # @see https://helpcenter.woodwing.com/hc/en-us/articles/206334175-Elvis-4-REST-API-checkout
+      #
+      # @param [String] assetId the Elvis ID of the asset to check out
+      # @param [Hash] options additional options for the request
+      # @option options [Boolean] :download whether to download the asset
+      #
+      # @return [Hash, String] a hash with the result of the API call, or a string with the contents of the file if it was succesfully downloaded
+      def checkout(assetId, options={})
+        url = base_url + "checkout/" + assetId
         response = get_response(url, options)
       end # checkout
 
-
-      # https://elvis.tenderapp.com/kb/api/rest-undo_checkout
-      def undo_checkout(options={})
-        url = base_url + "undo_checkout"
-        response = get_response(url, options)
+      # Cancel the checkout of an asset.
+      # @see https://helpcenter.woodwing.com/hc/en-us/articles/206334265-Elvis-4-REST-API-undo-checkout
+      #
+      # @param [String] assetId The Elvis ID of the asset to undo check out
+      #
+      # @return [Hash] a hash with the result of the API call
+      def undo_checkout(assetId)
+        url = base_url + "undocheckout/" + assetId
+        response = get_response(url)
       end # undo_checkout
 
       alias :abort_checkout :undo_checkout
