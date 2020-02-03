3778df0e69bc4386808c14d719a5da92157e26a6
David Posey <dgposey@gmail.com>
2020-02-03 14:32:00 -0600

    update auth keys methods to match api and bump version

--- lib/woodwing/elvis/rest/authorization_keys.rb
+++ lib/woodwing/elvis/rest/authorization_keys.rb
@@ -5,15 +5,15 @@ module WoodWing
 
       # https://elvis.tenderapp.com/kb/api/rest-createauthkey
       def create_auth_key(options={})
-        url = base_url + "create_auth_key"
-        response = get_response(url, options)
+        url = base_url + "createAuthKey"
+        response = get_response_using_post(url, options)
       end # create_auth_key
 
 
       # https://elvis.tenderapp.com/kb/api/rest-update_auth_key
       def update_auth_key(options={})
-        url = base_url + "update_auth_key"
-        response = get_response(url, options)
+        url = base_url + "updateAuthKey"
+        response = get_response_using_post(url, options)
       end # update_auth_key
 
       alias :replace_auth_key :update_auth_key
@@ -21,8 +21,8 @@ module WoodWing
 
       # https://elvis.tenderapp.com/kb/api/rest-revokeauthkeys
       def revoke_auth_keys(options={})
-        url = base_url + "revoke_auth_keys"
-        response = get_response(url, options)
+        url = base_url + "revokeAuthKeys"
+        response = get_response_using_post(url, options)
       end # revoke_auth_keys
 
 
